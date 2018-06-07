59449f627da238c84e53156fd3cbf05f11899f21
鬼雨 <634784732@qq.com>
2018-06-07 19:00:41 +0800

    AssetLoader

--- Assets/Scripts/AssetBundleFramework/AssetLoader.cs
+++ Assets/Scripts/AssetBundleFramework/AssetLoader.cs
@@ -2,15 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssetLoader : MonoBehaviour {
+public class AssetLoader : System.IDisposable
+{
+    private AssetBundle curAssetBundle;
+    private Hashtable assetHash;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AssetLoader(AssetBundle assetBundle)
+    {
+        if (assetBundle == null)
+        {
+            Debug.LogError("AssetBundle is null!");
+        }
+        else
+        {
+            curAssetBundle = assetBundle;
+        }
+    }
+
+    /// <summary>
+    /// 加载资源
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="assetName"></param>
+    /// <param name="isCache"></param>
+    /// <returns></returns>
+    public T LoadAsset<T>(string assetName, bool isCache = false) where T : UnityEngine.Object
+    {
+        if(assetHash.Contains(assetName))
+        {
+            return assetHash[assetName] as T;
+        }
+        T obj = curAssetBundle.LoadAsset<T>(assetName);
+        if (obj == null)
+            Debug.LogError("LoadAsset error....AssetBundle:" + curAssetBundle.name + "  assetName:" + assetName);
+        if (obj != null && isCache)
+        {
+            assetHash.Add(assetName, obj);
+        }
+        return obj;
+    }
+
+    /// <summary>
+    /// 卸载指定资源
+    /// </summary>
+    /// <param name="asset"></param>
+    /// <returns></returns>
+    public bool UnloadAsset(UnityEngine.Object asset)
+    {
+        if(asset != null)
+        {
+            Resources.UnloadAsset(asset);
+            return true;
+        }
+        return false;
+    }
+
+    public void Dispose()
+    {
+        curAssetBundle.Unload(false);
+    }
+
+    public void DisposeAll()
+    {
+        curAssetBundle.Unload(true);
+    }
+
+    public string[] GetAllAssetName()
+    {
+        return curAssetBundle.GetAllAssetNames();
+    }
 }
